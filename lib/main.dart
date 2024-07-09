f754291d6bfc23b716b2e263992f9c5d87fdd1d2
Andrew Virts <asvirts@outlook.com>
2024-07-09 17:23:28 -0500

    working on golden tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pomodoro/providers/settings_provider.dart';
 import 'package:pomodoro/screens/home.dart';
 
--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,18 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:pomodoro/main.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
-  testGoldens('Test tests', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ProviderScope(
-      child: MyApp(),
-    ));
-    await loadAppFonts();
+  testGoldens('Golden test - Home screen', (tester) async {
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: [
+        Device.phone,
+      ])
+      ..addScenario(
+        widget: const ProviderScope(
+          child: MyApp(),
+        ),
+        name: 'home page',
+      );
 
-    // Testing writing tests in Flutter
-    expect(find.text('pomodoro'), findsExactly(2));
-    expect(find.text('santa'), findsNothing);
+    await tester.pumpDeviceBuilder(builder);
+
+    await screenMatchesGolden(tester, 'screen-home_color-red_font-sans');
   });
 }
