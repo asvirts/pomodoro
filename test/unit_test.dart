41f2c2b2755b492762a23fb27920ed4f4a329e31
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2024-07-09 12:24:55 -0500

    Update unit_test.dart

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:pomodoro/main.dart';
-
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
